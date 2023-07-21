05c2650121251dfc12019ef6db3a3e58d6b54549
HasilbekR <hreymbaev@gmail.com>
2023-07-21 15:13:17 +0500

    Initial commit

---
+++ src/main/java/com/example/hybridbookingservice/HybridBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hybridbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybridBookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HybridBookingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hybridbookingservice/HybridBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hybridbookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridBookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
