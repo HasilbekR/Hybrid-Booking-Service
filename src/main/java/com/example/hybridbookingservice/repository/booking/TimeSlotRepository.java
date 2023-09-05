ae47901b51d1ae4d607d0e915fb8c0dc9ea6ab6c
HasilbekR <hreymbaev@gmail.com>
2023-09-05 19:40:39 +0500

    creating time slots for doctors updated. Now it won't store duplicate times

--- src/main/java/com/example/hybridbookingservice/repository/booking/TimeSlotRepository.java
+++ src/main/java/com/example/hybridbookingservice/repository/booking/TimeSlotRepository.java
@@ -8,15 +8,15 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TimeSlotRepository extends JpaRepository<TimeSlot, UUID> {
     @Query(value = "select t from timeslots t where t.availability = true and t.bookingDay = ?1 and t.doctorId = ?2 and (t.bookingDay <> ?4 or t.bookingTime >= ?3) order by t.bookingTime asc")
     List<TimeSlot> getDoctorAvailableTimeSlotForTheDay(LocalDate localDate, UUID doctorId, LocalTime now, LocalDate today);
-    @Query(value = "select t from timeslots t where t.availability = true  and t.bookingDay = ?1 and t.bookingTime = ?2 and t.doctorId = ?3")
-    Optional<TimeSlot> getAvailableTimeSlot(LocalDate localDate, LocalTime localTime, UUID doctorId);
+    @Query(value = "select t from timeslots t where t.bookingDay = ?1 and t.doctorId = ?2")
+    List<TimeSlot> getDoctorTimeSlotsForTheDay(LocalDate localDate, UUID doctorId);
     @Query(value = "select distinct t.bookingDay from timeslots t where t.bookingDay >= ?1 and t.doctorId = ?2 order by t.bookingDay asc limit 6")
     List<LocalDate> getWorkingDaysOfDoctor(LocalDate localDate, UUID doctorId);
+
 }
