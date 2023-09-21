032e8a21025420aebb3074fc80b2e8c1ada74c78
javohirjahonov <jahonovjavohir0@gmail.com>
2023-09-21 09:44:59 +0500

    bug fixed

--- src/main/java/com/example/hybridbookingservice/config/SecurityConfig.java
+++ src/main/java/com/example/hybridbookingservice/config/SecurityConfig.java
@@ -57,16 +57,6 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", corsConfiguration);
         return source;
     }
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource(){
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
-//        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-//        corsConfiguration.setAllowedHeaders(List.of("*"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", corsConfiguration);
-//        return source;
-//    }
 
 }
 
