b0e42ebc61ddc14e4529d81276c71cc7d9c34e53
javohirjahonov <jahonovjavohir0@gmail.com>
2023-09-21 17:15:05 +0500

    bug fixed

--- src/main/java/com/example/hybridbookingservice/repository/booking/BookingRepository.java
+++ src/main/java/com/example/hybridbookingservice/repository/booking/BookingRepository.java
@@ -10,9 +10,9 @@ import java.util.UUID;
 
 @Repository
 public interface BookingRepository extends JpaRepository<BookingEntity, UUID> {
-    @Query(value = "select b from bookings b where b.userId = ?1 and (b.status = 'COMPLETED' or b.status = 'DECLINED') order by b.createdDate desc ")
+    @Query(value = "select b from bookings b where b.userId = ?1 and (b.bookingStatus = 'COMPLETED' or b.bookingStatus = 'DECLINED') order by b.createdDate desc ")
     List<BookingEntity> getUserPastBookings(UUID userId);
-    @Query(value = "select b from bookings b where b.userId = ?1 and (b.status = 'SCHEDULED' or b.status = 'IN_PROGRESS') order by b.createdDate desc ")
+    @Query(value = "select b from bookings b where b.userId = ?1 and (b.bookingStatus = 'SCHEDULED' or b.bookingStatus = 'IN_PROGRESS') order by b.createdDate desc ")
     List<BookingEntity> getUserUpcomingBookings(UUID userId);
     @Query(value = "select b from bookings b where b.timeSlot.doctorId = ?1")
     List<BookingEntity> getDoctorBookings(UUID doctorId);
